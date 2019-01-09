bbebe055cff229bd8231d9e27fbed14e7a0093c7
Tom Tzook <tomtzook@gmail.com>
2019-01-09 21:55:36 +0200

    frombytes creation of cvimage

--- src/main/java/edu/flash3388/flashlib/vision/cv/CvImage.java
+++ src/main/java/edu/flash3388/flashlib/vision/cv/CvImage.java
@@ -3,15 +3,27 @@ package edu.flash3388.flashlib.vision.cv;
 import edu.flash3388.flashlib.vision.Image;
 import edu.flash3388.flashlib.vision.jpeg.JpegImage;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.core.MatOfInt;
+import org.opencv.imgcodecs.Imgcodecs;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class CvImage implements Image {
+
+    private static final Logger LOGGER = Logger.getLogger(CvImage.class.getName());
 
     private final Mat mMat;
 
     public CvImage(Mat mat) {
         mMat = mat;
+    }
+
+    public static CvImage fromBytes(byte[] bytes) {
+        Mat mat = Imgcodecs.imdecode(new MatOfByte(bytes), Imgcodecs.CV_LOAD_IMAGE_UNCHANGED);
+        return new CvImage(mat);
     }
 
     public Mat getMat() {
@@ -29,12 +41,28 @@ public class CvImage implements Image {
     }
 
     @Override
-    public byte[] getRaw() throws IOException {
-        return new byte[0];
+    public byte[] getRaw() {
+        MatOfByte buffer = new MatOfByte();
+        MatOfInt compressParams = new MatOfInt();
+
+        try {
+            Imgcodecs.imencode(".jpg", mMat, buffer, compressParams);
+            byte[] imageArr = new byte[(int) (buffer.total() * buffer.elemSize())];
+            buffer.get(0, 0, imageArr);
+            return imageArr;
+        } finally {
+            compressParams.release();
+        }
     }
 
     @Override
     public JpegImage toJpeg() {
-        return null;
+        try {
+            byte[] rawData = getRaw();
+            return JpegImage.fromBytes(rawData);
+        } catch (IOException e) {
+            LOGGER.log(Level.WARNING, "error exporting to jpeg", e);
+            return JpegImage.rgb(getHeight(), getWidth());
+        }
     }
 }
---
+++ src/main/java/edu/flash3388/flashlib/vision/cv/camera/CvCamera.java
@@ -0,0 +1,39 @@
+package edu.flash3388.flashlib.vision.cv.camera;
+
+import edu.flash3388.flashlib.vision.camera.Camera;
+import edu.flash3388.flashlib.vision.cv.CvImage;
+import org.opencv.core.Mat;
+import org.opencv.videoio.VideoCapture;
+import org.opencv.videoio.Videoio;
+
+public class CvCamera implements Camera<CvImage> {
+
+    private final VideoCapture mVideoCapture;
+
+    public CvCamera(VideoCapture videoCapture) {
+        mVideoCapture = videoCapture;
+    }
+
+    @Override
+    public int getFps() {
+        return (int) mVideoCapture.get(Videoio.CAP_PROP_FPS);
+    }
+
+    @Override
+    public int getHeight() {
+        return (int) mVideoCapture.get(Videoio.CAP_PROP_FRAME_HEIGHT);
+    }
+
+    @Override
+    public int getWidth() {
+        return (int) mVideoCapture.get(Videoio.CAP_PROP_FRAME_WIDTH);
+    }
+
+    @Override
+    public CvImage capture() {
+        Mat mat = new Mat();
+        mVideoCapture.read(mat);
+
+        return new CvImage(mat);
+    }
+}
