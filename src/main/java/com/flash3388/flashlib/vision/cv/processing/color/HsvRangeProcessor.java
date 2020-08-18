ed1a5e72db1a64e354ba03e68dd208024df42d41
Tom Tzook <tomtzook@gmail.com>
2020-08-18 21:43:44 +0300

    using color settings

--- src/main/java/com/flash3388/flashlib/vision/cv/processing/color/HsvRangeProcessor.java
+++ src/main/java/com/flash3388/flashlib/vision/cv/processing/color/HsvRangeProcessor.java
@@ -1,55 +1,34 @@
 package com.flash3388.flashlib.vision.cv.processing.color;
 
-import com.beans.Property;
-import com.beans.properties.atomic.AtomicProperty;
 import com.flash3388.flashlib.vision.cv.CvImage;
 import com.flash3388.flashlib.vision.cv.CvImageProcessor;
 import com.flash3388.flashlib.vision.cv.CvProcessing;
+import com.flash3388.flashlib.vision.processing.color.ColorRange;
+import com.flash3388.flashlib.vision.processing.color.HsvColorSettings;
 import org.opencv.core.Range;
 
 public class HsvRangeProcessor implements CvImageProcessor {
 
-    private final Property<Range> mHueRange;
-    private final Property<Range> mSaturationRange;
-    private final Property<Range> mValueRange;
-
+    private final HsvColorSettings mHsvColorSettings;
     private final CvProcessing mCvProcessing;
 
-    public HsvRangeProcessor(Property<Range> hueRange, Property<Range> saturationRange, Property<Range> valueRange, CvProcessing cvProcessing) {
-        mHueRange = hueRange;
-        mSaturationRange = saturationRange;
-        mValueRange = valueRange;
+    public HsvRangeProcessor(HsvColorSettings hsvColorSettings, CvProcessing cvProcessing) {
+        mHsvColorSettings = hsvColorSettings;
         mCvProcessing = cvProcessing;
-    }
-
-    public HsvRangeProcessor(Property<Range> hueRange, Property<Range> saturationRange, Property<Range> valueRange) {
-        this(hueRange, saturationRange, valueRange, new CvProcessing());
-    }
-
-    public HsvRangeProcessor(Range hueRange, Range saturationRange, Range valueRange) {
-        this(new AtomicProperty<>(hueRange), new AtomicProperty<>(saturationRange), new AtomicProperty<>(valueRange));
-    }
-
-    public Property<Range> hueRangeProperty() {
-        return mHueRange;
-    }
-
-    public Property<Range> saturationRangeProperty() {
-        return mSaturationRange;
-    }
-
-    public Property<Range> valueRangeProperty() {
-        return mValueRange;
     }
 
     @Override
     public CvImage process(CvImage cvImage) {
-        Range hue = mHueRange.get();
-        Range saturation = mSaturationRange.get();
-        Range value = mValueRange.get();
+        Range hue = colorRangeToRange(mHsvColorSettings.getHue());
+        Range saturation = colorRangeToRange(mHsvColorSettings.getSaturation());
+        Range value = colorRangeToRange(mHsvColorSettings.getValue());
 
         mCvProcessing.filterMatColors(cvImage.getMat(), cvImage.getMat(), hue, saturation, value);
 
         return cvImage;
+    }
+
+    private Range colorRangeToRange(ColorRange colorRange) {
+        return new Range(colorRange.getMin(), colorRange.getMax());
     }
 }
